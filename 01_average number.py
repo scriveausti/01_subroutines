aa16d5bc143aa43761cf81a5d647b44bb53683a8
scriveausti <scrive.austi@awatapu.school.nz>
2022-03-14 09:34:49 +1300

    averager

---
+++ 01_average number.py
@@ -0,0 +1,9 @@
+def averager(n1,n2,n3):
+    average = (n1 + n2 + n3) / 3
+    print("the average of {}, {} and {} is {}".format(n1,n2,n3,average))
+
+number1 = int(input("enter a number "))
+number2 = int(input("enter another number "))
+number3 = int(input("enter another number "))
+
+averager(number1,number2,number3)
